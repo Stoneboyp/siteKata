63370cf41feaaf6bdfd5004da94f851bd5992125
Stoneboyp <melnikovwor@gmail.com>
2022-09-16 22:12:50 +0300

    re1

--- brandmenu/script.js
+++ brandmenu/script.js
@@ -1,41 +1,21 @@
-const swiper = new Swiper(".  wiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: "true",
-    currentClass: "swiper-pagination-current",
-    dynamicBullets: true,
-  },
-});
-// const btn = document.querySelector(".btn");
-// const hidden = document.querySelector(".hidden");
+// const swiper = new Swiper(".swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "bullets",
+//     clickable: "true",
+//     currentClass: "swiper-pagination-current",
+//     dynamicBullets: true,
+//   },
+// });
+// function myFunction() {
+//   let x = document.querySelectorAll(".hidden");
+//   for (let i = 0; i < x.length; i++) {
+//     x[i].style.display = "block";
 
-// btn.addEventListener("click", btnClick);
-
-// function btnClick() {
-//   console.log(content.classList);
-
-//   if (hidden.classList.contains("hidden")) {
-//     btn.textContent = "Скрыть элемент";
-//   } else {
-//     btn.textContent = "Показать элемент";
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
 //   }
-
-//   hidden.classList.toggle("hidden");
-// }
-// const btn = document.querySelector(".btn");
-// const content = document.querySelector(".content");
-
-// btn.addEventListener("click", btnClick);
-
-// function btnClick() {
-//   console.log(content.classList);
-
-//   if (content.classList.contains("hidden")) {
-//     btn.textContent = "Скрыть элемент";
-//   } else {
-//     btn.textContent = "Показать элемент";
-//   }
-
-//   content.classList.toggle("hidden");
 // }
