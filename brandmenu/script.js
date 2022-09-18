4cbe39539f0da590e0c49a150d87c09b1bc121c9
Stoneboyp <melnikovwor@gmail.com>
2022-09-18 18:27:18 +0300

    pro2

--- brandmenu/script.js
+++ brandmenu/script.js
@@ -1,21 +1,32 @@
-// const swiper = new Swiper(".swiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "bullets",
-//     clickable: "true",
-//     currentClass: "swiper-pagination-current",
-//     dynamicBullets: true,
-//   },
-// });
-// function myFunction() {
-//   let x = document.querySelectorAll(".hidden");
-//   for (let i = 0; i < x.length; i++) {
-//     x[i].style.display = "block";
+function myFunction() {
+  let x = document.getElementById("hidden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   }
-// }
+let init = false;
+function swiperCard() {
+  if (window.innerWidth <= 768) {
+    if (!init) {
+      init = true;
+      let swiper = new Swiper(".swiper-container", {
+        direction: "horizontal",
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 32,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.enabled = false;
+    init = false;
+  }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);
